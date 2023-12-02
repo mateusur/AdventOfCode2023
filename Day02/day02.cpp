80a4822326a5099c62d6f379cf77bc1508253e2b
Mateusz Urbaniak <urban1998@wp.pl>
2023-12-02 15:21:32 +0100

    Add solution to day2

--- Day02/day02.cpp
+++ Day02/day02.cpp
@@ -8,26 +8,21 @@
 using namespace std;
 
 void read_from_file(const string& filename, stringstream& buffer) {
-  ifstream file;
-  file.open(filename);
-  if(!file){
-    		cout << "Error opening the file, the program will end with no file.\n";
-
-  }
-
+  ifstream file(filename);
   if (file) {
     buffer << file.rdbuf();
     file.close();
   }
 }
-uint64_t part1() {
+
+int part1() {
   std::stringstream buffer;
-  read_from_file("input.txt", buffer);
+  read_from_file("/home/mateusz/AdventOfCode2023/Day02/input.txt", buffer);
   vector<int> values;
   string line;
   const int max_blue  =14;
   const int max_green = 13;
-  const int max_red = 11; 
+  const int max_red = 12; 
   long sum = 0;
   int game =0;
   int number;
@@ -37,6 +32,8 @@ uint64_t part1() {
     if(line == "Game"){
       if(possible){
         sum += game;
+      }else{
+        possible = true;
       }
       game++;
       ignore = true;
@@ -65,13 +62,59 @@ uint64_t part1() {
       }
     }
   }
+  if(possible){
+    sum += game;
+  }
+  return sum;
+}
+int part2() {
+  std::stringstream buffer;
+  read_from_file("/home/mateusz/AdventOfCode2023/Day02/input.txt", buffer);
+  vector<int> values;
+  string line;
+   int max_blue  =0;
+   int max_green = 0;
+   int max_red = 0; 
+  long sum = 0;
+  int number;
+  bool ignore = true;
+  while (buffer >> line) {
+    if(line == "Game"){
+      sum += max_blue*max_red*max_green;
+      max_blue  =0;
+      max_green = 0;
+     max_red = 0; 
+      ignore = true;
+    }
+    else if(line == "blue," || line == "blue;" || line == "blue"){
+      if(number > max_blue){
+        max_blue = number;
+      }
+    }
+    else if(line == "red," || line == "red;" || line == "red"){
+      if(number > max_red){
+        max_red = number;
+      }
+    }
+    else if(line == "green," || line == "green;" || line == "green"){
+      if(number > max_green){
+        max_green = number;
+      }
+    }
+    else{
+      if(!ignore){
+        number = stoi(line);
+      }
+      else{
+        ignore = false;
+      }
+    }
+  }
+  
+  sum += max_blue*max_red*max_green;
   return sum;
 }
 int main() {
-    ofstream myfile;
-  myfile.open ("example.txt");
-  myfile << "Writing this to a file.\n";
-  myfile.close();
-  return 0;
-  cout << part1() << endl;
+  // cout << part1() << endl;
+  cout << part2() << endl;
 }
